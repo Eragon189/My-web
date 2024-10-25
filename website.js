e9ed7089258959ff03666b01a882a0b7a9dd8331
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2024-10-25 12:51:57 -0700

    Add code to website.js

---
+++ website.js
@@ -0,0 +1,33 @@
+
+
+
+
+let url = "";
+let lastName;
+let firstName;
+let passKey;
+
+document.getElementById("button").onclick =  function() {
+firstName= document.getElementById("firstName").value;
+lastName = document.getElementById("lastName").value;
+document.getElementById("counters").innerHtml = `${firstName}, ${lastName}`
+}
+
+
+
+
+function sendData(firstName,lastName,passKey,url) {
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        firstNameOfbullier: firstName,
+        lastNameOfbullier: lastName
+    })
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+}
