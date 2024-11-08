f8d8f40d7eb9a00e8702fe5f089d58f5485cbe45
Eragon189 <144192904+Eragon189@users.noreply.github.com>
2024-11-08 02:33:26 +0000

    make code pritier

--- website.js
+++ website.js
@@ -1,33 +1,26 @@
-
-
-
-
 let url = "";
 let lastName;
 let firstName;
 let passKey;
+console.log("output works!!!!")
+document.getElementById("button").onclick = function () {
+  firstName = document.getElementById("firstName").value;
+  lastName = document.getElementById("lastName").value;
+  document.getElementById("counters").innerHtml = `${firstName}, ${lastName}`;
+};
 
-document.getElementById("button").onclick =  function() {
-firstName= document.getElementById("firstName").value;
-lastName = document.getElementById("lastName").value;
-document.getElementById("counters").innerHtml = `${firstName}, ${lastName}`
-}
-
-
-
-
-function sendData(firstName,lastName,passKey,url) {
-fetch(url, {
-    method: 'POST',
+function sendData(firstName, lastName, passKey, url) {
+  fetch(url, {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-        firstNameOfbullier: firstName,
-        lastNameOfbullier: lastName
-    })
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+      firstNameOfbullier: firstName,
+      lastNameOfbullier: lastName,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 }
